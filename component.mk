f8dc4429cd76c6c2b61c4a06894e54bd07a8de6c
Patrik Berglund <patrik.d.berglund@gmail.com>
2018-12-26 22:37:21 +0100

    initial

---
+++ component.mk
@@ -0,0 +1,109 @@
+#
+# Component Makefile
+#
+ 
+# Component configuration in preprocessor defines
+CFLAGS += -DUSE_LWIP_SOCKET_FOR_AZURE_IOT
+CFLAGS += -Wno-unused-function -Wno-missing-braces -Wno-missing-field-initializers
+
+COMPONENT_ADD_INCLUDEDIRS := \
+port \
+port/inc \
+azure-iot-sdk-c/certs \
+azure-iot-sdk-c/c-utility/inc \
+azure-iot-sdk-c/c-utility/inc/azure_c_shared_utility \
+azure-iot-sdk-c/c-utility/pal/inc \
+azure-iot-sdk-c/c-utility/pal/freertos \
+azure-iot-sdk-c/c-utility/pal/generic \
+azure-iot-sdk-c/iothub_client/inc \
+azure-iot-sdk-c/umqtt/inc \
+azure-iot-sdk-c/umqtt/inc/azure_umqtt_c \
+azure-iot-sdk-c/deps/parson \
+ 
+ 
+COMPONENT_OBJS = \
+azure-iot-sdk-c/certs/certs.o \
+\
+\
+azure-iot-sdk-c/c-utility/pal/freertos/lock.o \
+azure-iot-sdk-c/c-utility/pal/dns_async.o \
+azure-iot-sdk-c/c-utility/pal/socket_async.o \
+azure-iot-sdk-c/c-utility/pal/freertos/threadapi.o \
+azure-iot-sdk-c/c-utility/pal/freertos/tickcounter.o \
+azure-iot-sdk-c/c-utility/pal/tlsio_options.o \
+azure-iot-sdk-c/c-utility/pal/lwip/sntp_lwip.o	\
+\
+\
+azure-iot-sdk-c/c-utility/src/http_proxy_io.o \
+azure-iot-sdk-c/c-utility/src/base32.o \
+azure-iot-sdk-c/c-utility/adapters/agenttime.o	\
+\
+\
+port/src/platform_openssl_compact.o \
+port/src/tlsio_esp_tls.o \
+\
+\
+azure-iot-sdk-c/c-utility/src/xlogging.o \
+azure-iot-sdk-c/c-utility/src/singlylinkedlist.o \
+azure-iot-sdk-c/c-utility/src/buffer.o \
+azure-iot-sdk-c/c-utility/src/consolelogger.o \
+azure-iot-sdk-c/c-utility/src/constbuffer.o \
+azure-iot-sdk-c/c-utility/src/constmap.o \
+azure-iot-sdk-c/c-utility/src/crt_abstractions.o \
+azure-iot-sdk-c/c-utility/src/doublylinkedlist.o \
+azure-iot-sdk-c/c-utility/src/gballoc.o \
+azure-iot-sdk-c/c-utility/src/gb_stdio.o \
+azure-iot-sdk-c/c-utility/src/gb_time.o \
+azure-iot-sdk-c/c-utility/src/hmac.o \
+azure-iot-sdk-c/c-utility/src/hmacsha256.o \
+azure-iot-sdk-c/c-utility/src/httpapiex.o \
+azure-iot-sdk-c/c-utility/src/httpapiexsas.o \
+azure-iot-sdk-c/c-utility/src/httpheaders.o \
+azure-iot-sdk-c/c-utility/src/map.o \
+azure-iot-sdk-c/c-utility/src/optionhandler.o \
+azure-iot-sdk-c/c-utility/src/sastoken.o \
+azure-iot-sdk-c/c-utility/src/sha1.o \
+azure-iot-sdk-c/c-utility/src/sha224.o \
+azure-iot-sdk-c/c-utility/src/sha384-512.o \
+azure-iot-sdk-c/c-utility/src/strings.o \
+azure-iot-sdk-c/c-utility/src/string_tokenizer.o \
+azure-iot-sdk-c/c-utility/src/urlencode.o \
+azure-iot-sdk-c/c-utility/src/usha.o \
+azure-iot-sdk-c/c-utility/src/vector.o \
+azure-iot-sdk-c/c-utility/src/xio.o \
+azure-iot-sdk-c/c-utility/src/base64.o \
+\
+\
+azure-iot-sdk-c/iothub_client/src/iothub_client_ll.o \
+azure-iot-sdk-c/iothub_client/src/iothub_client_core_ll.o \
+azure-iot-sdk-c/iothub_client/src/iothub_client_ll_uploadtoblob.o \
+azure-iot-sdk-c/iothub_client/src/iothub_client_authorization.o \
+azure-iot-sdk-c/iothub_client/src/iothub_client_retry_control.o \
+azure-iot-sdk-c/iothub_client/src/iothub_client_diagnostic.o \
+azure-iot-sdk-c/iothub_client/src/iothub_message.o \
+azure-iot-sdk-c/iothub_client/src/iothubtransport.o \
+azure-iot-sdk-c/iothub_client/src/iothubtransportmqtt.o \
+azure-iot-sdk-c/iothub_client/src/iothubtransport_mqtt_common.o \
+azure-iot-sdk-c/iothub_client/src/iothub_transport_ll_private.o \
+azure-iot-sdk-c/iothub_client/src/version.o \
+\
+\
+azure-iot-sdk-c/umqtt/src/mqtt_client.o \
+azure-iot-sdk-c/umqtt/src/mqtt_codec.o \
+azure-iot-sdk-c/umqtt/src/mqtt_message.o \
+\
+\
+azure-iot-sdk-c/deps/parson/parson.o \
+
+COMPONENT_SRCDIRS := \
+port/src \
+azure-iot-sdk-c/certs \
+azure-iot-sdk-c/c-utility/pal \
+azure-iot-sdk-c/c-utility/pal/freertos \
+azure-iot-sdk-c/c-utility/pal/lwip \
+azure-iot-sdk-c/c-utility/src \
+azure-iot-sdk-c/c-utility/adapters \
+azure-iot-sdk-c/umqtt/src \
+azure-iot-sdk-c/iothub_client/src \
+azure-iot-sdk-c/deps/parson \
+
---
+++ examples/iothub_client_sample_mqtt/main/iothub_client_sample_mqtt.c
@@ -0,0 +1,101 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "iothub_client_ll.h"
+#include "iothub_device_client_ll.h"
+#include "iothub_client_options.h"
+#include "iothub_message.h"
+#include "azure_c_shared_utility/threadapi.h"
+#include "azure_c_shared_utility/crt_abstractions.h"
+#include "azure_c_shared_utility/platform.h"
+#include "azure_c_shared_utility/shared_util_options.h"
+#include "iothubtransportmqtt.h"
+#include "iothub_client_options.h"
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "certs.h"
+
+static const char *TAG = "IOTHUB";
+
+#define EXAMPLE_IOTHUB_CONNECTION_STRING CONFIG_IOTHUB_CONNECTION_STRING
+static const char *connectionString = EXAMPLE_IOTHUB_CONNECTION_STRING;
+
+static void connection_status_callback(IOTHUB_CLIENT_CONNECTION_STATUS status, IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason, void *userContextCallback)
+{
+    ESP_LOGI(TAG, "Connection Status Callback: status=%d reason=%d", status, reason);
+
+    switch (reason)
+    {
+    case IOTHUB_CLIENT_CONNECTION_OK:
+        ESP_LOGI(TAG, "IOTHUB_CLIENT_CONNECTION_OK");
+        break;
+    case IOTHUB_CLIENT_CONNECTION_EXPIRED_SAS_TOKEN:
+        ESP_LOGW(TAG, "IOTHUB_CLIENT_CONNECTION_EXPIRED_SAS_TOKEN");
+        break;
+    case IOTHUB_CLIENT_CONNECTION_DEVICE_DISABLED:
+        ESP_LOGW(TAG, "IOTHUB_CLIENT_CONNECTION_DEVICE_DISABLED");
+        break;
+    case IOTHUB_CLIENT_CONNECTION_BAD_CREDENTIAL:
+        ESP_LOGW(TAG, "IOTHUB_CLIENT_CONNECTION_BAD_CREDENTIAL");
+        break;
+    case IOTHUB_CLIENT_CONNECTION_RETRY_EXPIRED:
+        ESP_LOGW(TAG, "IOTHUB_CLIENT_CONNECTION_RETRY_EXPIRED");
+        break;
+    case IOTHUB_CLIENT_CONNECTION_NO_NETWORK:
+        ESP_LOGW(TAG, "IOTHUB_CLIENT_CONNECTION_NO_NETWORK");
+        break;
+    case IOTHUB_CLIENT_CONNECTION_COMMUNICATION_ERROR:
+        ESP_LOGW(TAG, "IOTHUB_CLIENT_CONNECTION_COMMUNICATION_ERROR");
+        break;
+    default:
+        ESP_LOGW(TAG, "UNKNOWN_REASON");
+    }
+}
+
+void iothub_client_sample_mqtt_run(void)
+{
+    IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle;
+
+    if (platform_init() != 0)
+    {
+        ESP_LOGE(TAG, "Failed to initialize the platform.");
+    }
+    else
+    {
+        if ((iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, MQTT_Protocol)) == NULL)
+        {
+            ESP_LOGE(TAG, "iotHubClientHandle is NULL!");
+        }
+        else if (IoTHubClient_LL_SetConnectionStatusCallback(iotHubClientHandle, connection_status_callback, NULL) != IOTHUB_CLIENT_OK)
+        {
+            ESP_LOGE(TAG, "Failed on IoTHubClient_LL_SetConnectionStatusCallback");
+        }
+        else
+        {
+            bool traceOn = true;
+            IoTHubClient_LL_SetOption(iotHubClientHandle, OPTION_LOG_TRACE, &traceOn);
+            IoTHubClient_LL_SetOption(iotHubClientHandle, OPTION_TRUSTED_CERT, certificates);
+
+            do
+            {
+                IoTHubClient_LL_DoWork(iotHubClientHandle);
+                ThreadAPI_Sleep(10);
+            } while (true);
+
+            IoTHubClient_LL_Destroy(iotHubClientHandle);
+        }
+        platform_deinit();
+    }
+}
+
+int main(void)
+{
+    iothub_client_sample_mqtt_run();
+    return 0;
+}
